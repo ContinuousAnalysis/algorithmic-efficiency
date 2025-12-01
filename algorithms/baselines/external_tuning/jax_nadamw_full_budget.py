6f7d638adc190d9bce3f30ba3314c27dac1a8cc5
rka97 <akregeb@gmail.com>
2025-12-01 04:33:49 +0000

    ImageNet and CIFAR mixed-precision support, need to debug slow pytorch
    - Introduced DTYPE enum to standardize data types (FLOAT32, FLOAT16, BFLOAT16) for JAX and PyTorch.
    - Updated input pipelines and model definitions in CIFAR and ImageNet workloads to utilize mixed precision.
    - Implemented casting policies for parameters and inputs using jmp and torch.autocast.

--- algorithms/baselines/external_tuning/jax_nadamw_full_budget.py
+++ algorithms/baselines/external_tuning/jax_nadamw_full_budget.py
@@ -396,6 +396,8 @@ def get_batch_size(workload_name):
     return 128
   elif workload_name == 'mnist':
     return 16
+  elif workload_name == 'cifar':
+    return 16384
   else:
     raise ValueError(f'Unsupported workload name: {workload_name}.')
 
